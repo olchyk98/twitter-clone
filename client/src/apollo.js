a3b08225d0540f467b89e5de276602f6642dd315
oles <olchyk89@gmail.com>
2018-10-23 13:18:19 +0200

    AS

--- client/src/apollo.js
+++ client/src/apollo.js
@@ -2,27 +2,19 @@ import { ApolloClient } from 'apollo-boost';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { WebSocketLink } from 'apollo-link-ws';
 import { createUploadLink } from 'apollo-upload-client';
-// import Retry from 'apollo-link-retry';
-import { ApolloLink } from 'apollo-link'
+import { ApolloLink } from 'apollo-link';
 
 import { apiPath, wssPath } from './apiPath';
 
 const client = new ApolloClient({
-  link: ApolloLink.from([
-  	// new Retry(),
-  	new WebSocketLink({
-      uri: `${ wssPath }/graphql`,
-      options: { reconnect: true }
-    }),
-    createUploadLink({ uri: `${ apiPath }/graphql` })
-  ]),
-  cache: new InMemoryCache()
+	link: ApolloLink.from([ // FIXME: upload link is not working
+		new WebSocketLink({
+     		uri: `${ wssPath }/graphql`,
+      		options: { reconnect: true }
+    	}),
+		createUploadLink({ uri: `${ apiPath }/graphql` })
+	]),
+	cache: new InMemoryCache()
 });
 
 export default client;
-
-// new WebSocketLink({
-//   uri: `${ wssPath }/graphql`,
-//   options: { reconnect: true }
-// }),
-// createUploadLink({ uri: `${ apiPath }/graphql` })
